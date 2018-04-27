3677c59dfec933126d8ef84c064a90b392434ec2
aprabh <aprabh@juniper.net>
2018-04-27 19:16:36 -0400

    hex/raw data send

--- scapy_traffic_gen.py
+++ scapy_traffic_gen.py
@@ -1,16 +1,17 @@
 '''
+Author: Aravind Srinivas S.P
+
 To Do:
-1. use Hex frame directly to send packet
 2. read pcap file and dump info as a json format  
 3. exit traffic gen app unpon ENTER key encounter (useful when someone runs with time 10000s and want to close half way)
 4. find multiplier for generation of rate (currently 100000 loop gives ~33Mbps)
 '''
 
-import socket
 import os
 import argparse
 import subprocess
 import json
+import binascii
 
 ########################################
 # check if scapy module is installed
@@ -28,9 +29,9 @@ from scapy.utils import rdpcap
 from scapy.utils import wrpcap
 
 parser = argparse.ArgumentParser()
-#parser.add_option('--RP', '--rdpcap', action='store', dest='read_pcap', help="read pcap file", default="False")
-parser.add_argument('--WP','--wrpcap', action='store_true',dest='write_pcap',default="False", help='write to pcap file')
-parser.add_argument('--T', '--time', action='store', dest='time', help ='time duration for which traffic should pass', default = 1)
+#parser.add_option('--RP', '-rdpcap', action='store', dest='read_pcap', help="read pcap file", default="False")
+parser.add_argument('--WP','-wrpcap', action='store_true',dest='write_pcap',default="False", help='write to pcap file')
+parser.add_argument('--T', '-time', action='store', dest='time', help ='time duration for which traffic should pass', default = 1)
 args = parser.parse_args()
 
 
@@ -41,15 +42,23 @@ args = parser.parse_args()
 class packet:
 	
 	def __init__(self,trans,flow):
-		self.flow = int(flow)
 		self.trans = str(trans)
-		if self.trans == 'ICMP':
-			self.pkt = Ether()/IP()/ICMP()/Raw()
-		if self.trans == 'TCP':
-			self.pkt = Ether()/IP()/TCP()/Raw()
-		if self.trans =='UDP':
-			self.pkt = Ether()/IP()/UDP()/Raw()
-		
+		self.flow = str(flow)
+		print(self.trans)
+		print(self.flow)
+		if ((self.trans != 'NONE') and (self.flow != 'NONE')):
+			self.flow = int(self.flow)
+
+			if ((self.trans == 'ICMP') and (self.flow != 1)):
+				print("wrong value received, if you want multiple flows,please use TCP/UDP and if you want to use ICMP please use flow:1 in json")
+				exit()
+			elif self.trans == 'ICMP':
+				self.pkt = Ether()/IP()/ICMP()/Raw()
+			elif self.trans == 'TCP':
+				self.pkt = Ether()/IP()/TCP()/Raw()
+			elif self.trans =='UDP':
+				self.pkt = Ether()/IP()/UDP()/Raw()
+			
 	def create(self,src_mac,dst_mac,src_ip,dst_ip,payload):
 		'''
 		write the packet created into a pcap
@@ -91,15 +100,32 @@ class packet:
 			print ("wrong value received, if you want multiple flows, please use TCP/UDP and if you want to use ICMP please use flow:1 in json")
 			exit() 
 		
+	def rawhexsend(self,data,loop,iface):
+		"""
+		send raw hex data 
+		"""
+		type = data[:3]	
+
+		if type=="raw":
+			self.pkt = Ether(data[4:])
+			sendpfast(self.pkt, loop=int(loop), iface=iface)
+
+		if type=="hex":
+			self.payload = binascii.unhexlify(data[4:])
+			self.pkt = Ether(self.payload)
+			sendpfast(self.pkt, loop=int(loop), iface=iface)
+		
 	
 	#def read_pcap(self,file_name):
-		'''
+		"""
 		To Do
 		read pcap and dump information/modify packet and recreate pcap 
-		'''
+		"""
 	
 	def send(self,pkt,loop,iface,time):
-		''' send packets using scapy by reading pcap '''
+		"""
+		 send packets using scapy by reading pcap 
+		"""
 		self.read_packet = rdpcap(pkt)
 		i= 0
 		while i < int(time):
@@ -107,21 +133,28 @@ class packet:
 			i = i +1
 
 def Main():
-	'''
-	To Do
-	check if necessary modules are installed,
-	'''
 	data = json.load(open('input_json.json'))
-	pkt = packet(data['trns'],data['flow'])
-	traf = pkt.create(data['srcmac'],data['dstmac'],data['srcip'],data['dstip'], data['payload'])
-	pkt.send(traf, data['loop'], data['intf'], args.time)
 	
-	#################################################
-	# delete pcap file if write pcap option not chosen
-	##################################################
+	if ((data['trns']=='NONE') and (data['flow']=='NONE')):
+		if data['raw_hex_load']:
+			"""
+			Need mention both trns and flow as 'NONE' in the json file
+			"""
+			pkt = packet(data['trns'],data['flow'])
+			traf = pkt.rawhexsend(data['raw_hex_load'], data['loop'], data['intf'])
+		else:
+			print("enter hex data under key:raw_hex_load in the json file")
+	else:
+		pkt = packet(data['trns'],data['flow'])
+		traf = pkt.create(data['srcmac'],data['dstmac'],data['srcip'],data['dstip'], data['payload'])
+		pkt.send(traf, data['loop'], data['intf'], args.time)
 	
-	if args.write_pcap == 'False':
-		os.system('rm {}'.format(traf))
+		#################################################
+		# delete pcap file if write pcap option not chosen
+		##################################################
+	
+		if args.write_pcap == 'False':
+			os.system('rm {}'.format(traf))
 
 if __name__ == '__main__':
 	Main()
